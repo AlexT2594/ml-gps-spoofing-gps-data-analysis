fbbb4c559dc8e0be6fe8515599579a4199c7b9c0
Alexandru Daniel Tufa <alex.tufa94@gmail.com>
2019-04-30 11:11:00 +0200

    Added elements for evaluating the numerical algorithms

--- ml/AttackSimNumeric.py
+++ ml/AttackSimNumeric.py
@@ -19,24 +19,17 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 from utils.data import get_numeric_data_from_file
 from random import shuffle
+import random
+
+from keras.callbacks import EarlyStopping
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
 
 def main():
 
     N_SAT = 32
 
-    names = ["Logistic Regression", "Nearest Neighbors", "Linear SVM", "RBF SVM",
-             "Decision Tree", "Random Forest", "Neural Net", "AdaBoost"]
-
-    classifiers = [
-        LogisticRegression(solver="liblinear", multi_class='auto'),
-        KNeighborsClassifier(),
-        SVC(kernel="linear", C=0.025),
-        SVC(gamma=2, C=1),
-        DecisionTreeClassifier(max_depth=5),
-        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-        MLPClassifier(solver="lbfgs"),
-        AdaBoostClassifier()
-    ]
+    names = ["Logistic Regression", "Linear SVM", "Neural Net"]
 
     # We create the preprocessing pipelines for numeric data
     numeric_features = []
@@ -60,6 +53,7 @@ def main():
     data_train = pd.read_csv(data_train_file_name)
 
     X_train = data_train.drop('spoofed', axis=1)
+    #y_train = y_train_NN = [random.randint(0, 1) for _ in range(len(data_train['spoofed']))]
     y_train = data_train['spoofed']
 
     data_test_file_name = "../data/numeric_eval/data_test.csv"
@@ -68,17 +62,54 @@ def main():
     X_test = data_test.drop('spoofed', axis=1)
     y_test = data_test['spoofed']
 
-    # iterate over classifiers
-    for name, classifier in zip(names, classifiers):
-        print("==> Classifier: " + name)
-        clf = Pipeline(steps=[('preprocessor', preprocessor),
-                              ('classifier', classifier)])
+    X_train_NN = preprocessor.fit_transform(X_train)
+    y_train_NN = y_train.values
 
-        clf.fit(X_train, y_train)
+
+    n_cols = X_train_NN.shape[1]
+
+    model = Sequential()
+    model.add(Dense(64, activation='relu', input_dim=n_cols))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    classifiers_init = [
+        LogisticRegression(solver="liblinear", multi_class='auto'),
+        SVC(kernel="linear", C=0.025),
+        model
+    ]
+
+
+    # iterate over classifiers
+    for name, classifier in zip(names, classifiers_init):
+        print("==> Classifier: " + name)
+
+        if name == "Neural Net":
+            classifier.fit(X_train_NN, y_train_NN, epochs=10, callbacks=[EarlyStopping(monitor='loss', patience=3)])
+            classifier._make_predict_function()
+            clf = classifier
+
+        else:
+            clf = Pipeline(steps=[('preprocessor', preprocessor),
+                                  ('classifier', classifier)])
+            clf.fit(X_train, y_train)
+
+
+
         pred = clf.predict(X_test)
 
+        #if name == "Neural Net":
+        #    pred = [round(_[0]) for _ in clf.predict(X_test)]
+
+
         score = accuracy_score(y_test, pred)
-        print("Accuracy:   %0.3f" % score)
+        print("\tAccuracy:   %0.3f\n" % score)
 
 
 if __name__ == '__main__':
